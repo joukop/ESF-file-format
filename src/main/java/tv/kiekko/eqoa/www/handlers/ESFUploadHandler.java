c2bb5406cc65065b509a56638eab957ec5f9daa7
Joshua Cole <jcolechanged@gmail.com>
2021-03-20 20:26:26 -0700

    Setup upload handler to parse uploaded files.

--- src/main/java/tv/kiekko/eqoa/www/handlers/ESFUploadHandler.java
+++ src/main/java/tv/kiekko/eqoa/www/handlers/ESFUploadHandler.java
@@ -2,33 +2,63 @@ package tv.kiekko.eqoa.www.handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.RequestContext;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ESFUploadHandler implements HttpHandler {
 
      @Override
-     public void handle(HttpExchange he) throws IOException {
-         // parse request
-         Map<String, Object> parameters = new HashMap<String, Object>();
-         InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
-         BufferedReader br = new BufferedReader(isr);
-         String query = br.readLine();
-         //parseQuery(query, parameters);
-
-         // send response
-         String response = "";
-         for (String key : parameters.keySet()) {
-             response += key + " = " + parameters.get(key) + "\n";
+     public void handle(HttpExchange t) throws IOException {
+         for(Map.Entry<String, List<String>> header : t.getRequestHeaders().entrySet()) {
+             System.out.println(header.getKey() + ": " + header.getValue().get(0));
          }
-         he.sendResponseHeaders(200, response.length());
-         OutputStream os = he.getResponseBody();
-         os.write(response.toString().getBytes());
-         os.close();
+         DiskFileItemFactory d = new DiskFileItemFactory();
+
+         try {
+             ServletFileUpload up = new ServletFileUpload(d);
+             List<FileItem> result = up.parseRequest(new RequestContext() {
+
+                 @Override
+                 public String getCharacterEncoding() {
+                     return "UTF-8";
+                 }
+
+                 @Override
+                 public int getContentLength() {
+                     return 0; //tested to work with 0 as return
+                 }
+
+                 @Override
+                 public String getContentType() {
+                     return t.getRequestHeaders().getFirst("Content-type");
+                 }
+
+                 @Override
+                 public InputStream getInputStream() throws IOException {
+                     return t.getRequestBody();
+                 }
+
+             });
+             t.getResponseHeaders().add("Content-type", "text/plain");
+             t.sendResponseHeaders(200, 0);
+             OutputStream os = t.getResponseBody();
+             for(FileItem fi : result) {
+                 os.write(fi.getName().getBytes());
+                 os.write("\r\n".getBytes());
+                 System.out.println("File-Item: " + fi.getFieldName() + " = " + fi.getName());
+             }
+             os.close();
+
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
      }
 }
